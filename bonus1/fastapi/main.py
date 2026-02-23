af51e0c4cff8c0198c9bbe74219247925602eca1
00Bhuwan <9843lalitpur@gmail.com>
2026-02-23 12:35:51 +0545

    added fastapi

--- bonus1/fastapi/main.py
+++ bonus1/fastapi/main.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.get("/")
 async def get_all_todos():
-    data = collection.find()
+    data = collection.find({"is_deleted": False})
     return all_tasks(data)
 
 @router.post("/")
@@ -22,16 +22,34 @@ async def create_task(new_task: Todo):
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Some error occured {e}")
     
-@app.router("/{task_id}")
+@router.put("/{task_id}")
 async def update_task(task_id: str, updated_task: Todo):
     try:
         id = ObjectId(task_id)
-        exesting_doc = collection.find_one({"id": id, "is_deleted": False})
+        exesting_doc = collection.find_one({"_id": id, "is_deleted": False})
         if not exesting_doc:
-            return HTTPException(status_code=401, detail="Task doesn't exist.")
+            return HTTPException(status_code=404, detail="Task doesn't exist.")
+        
         updated_task.updated_at = datetime.timestamp(datetime.now())
-        resp = collection.update_one({"id":id}, {"$set": dict(updated_task)})
+        resp = collection.update_one({"_id":id}, {"$set": dict(updated_task)})
+        
         return {"status_code": 200, "message": "Task Updated Successfully"}
+    
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Some error occured {e}.")
+
+@router.delete("/{task_id}")
+async def delete_task(task_id: str):
+    try:
+        id = ObjectId(task_id)
+        exesting_doc = collection.find_one({"_id": id, "is_deleted": False})
+        if not exesting_doc:
+            return HTTPException(status_code=404, detail="Task doesn't exist.")
+        
+        resp = collection.update_one({"_id":id}, {"$set": {"is_deleted": True}})
+        
+        return {"status_code": 200, "message": "Task Deleted Successfully"}
+    
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Some error occured {e}.")
 
